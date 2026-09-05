1577c1f09c5ad3a079ffbb421b3e7bf21ca5c762
Amit Sinha <amitsinhadeveloper@gmail.com>
2026-09-05 16:23:58 +0530

    feat(security): prevent screen recording and screenshots on mobile

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,10 +7,29 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    let result = super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    preventScreenCapture()
+    return result
   }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  }
+
+  private func preventScreenCapture() {
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(screenCaptureChanged),
+      name: UIScreen.capturedDidChangeNotification,
+      object: nil
+    )
+  }
+
+  @objc private func screenCaptureChanged() {
+    if UIScreen.main.isCaptured {
+      window?.isHidden = true
+    } else {
+      window?.isHidden = false
+    }
   }
 }
