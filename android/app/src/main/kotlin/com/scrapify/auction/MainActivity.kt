09315e7fb914eb302df532128d1f0e2908b1418f
Amit Sinha <amitsinhadeveloper@gmail.com>
2026-09-23 22:28:57 +0530

    Allow screenshots in v1 release

--- android/app/src/main/kotlin/com/scrapify/auction/MainActivity.kt
+++ android/app/src/main/kotlin/com/scrapify/auction/MainActivity.kt
@@ -1,20 +1,10 @@
 package com.scrapify.auction
 
 import android.os.Bundle
-import android.view.WindowManager
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity : FlutterFragmentActivity() {
-    // Prevent screenshots and screen recording in the production app.
-    private val screenCaptureProtectionEnabled = true
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        if (screenCaptureProtectionEnabled) {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_SECURE,
-                WindowManager.LayoutParams.FLAG_SECURE
-            )
-        }
         super.onCreate(savedInstanceState)
     }
 }
